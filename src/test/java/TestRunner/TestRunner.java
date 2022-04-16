e1108417c0f4053528e7db2229a8f3f2c5739dd6
SalemD <diaa.alharef@gmail.com>
2022-04-16 22:31:20 +0200

    Changes Done

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/Features",
-        glue = {"StepDefinition"})
+        glue = {"StepDefinition"},
+        plugin ={"pretty","html:target/site/cucumber-pretty","json:target/cucumber.json","junit:target/cucumber.xml"})
 
 public class TestRunner {
 
