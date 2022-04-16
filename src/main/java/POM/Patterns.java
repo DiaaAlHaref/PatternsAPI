6255d183a7d332437ac61363a165264d29a2b19d
SalemD <diaa.alharef@gmail.com>
2022-04-17 01:50:13 +0200

    Last Commit

--- src/main/java/POM/Patterns.java
+++ src/main/java/POM/Patterns.java
@@ -5,19 +5,18 @@ import FileReader.PropertiesFile;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
 public class Patterns {
 
-    boolean flag;
-    SoftAssert softAssert = new SoftAssert();
 
-    private String[] data = PropertiesFile.propertiesFileReader(new String[]{"BaseUrl","User_Agent","User_Agent_Type"});
-    private String BaseUrl = data[0];
-    private String User_Agent= data[1];
-    private String User_Agent_Type= data[2];
+
+
+    private final String[] data = PropertiesFile.propertiesFileReader(new String[]{"BaseUrl","User_Agent","User_Agent_Type"});
+    private final String BaseUrl = data[0];
+    private final String User_Agent= data[1];
+    private final String User_Agent_Type= data[2];
 
     public void BaseURL(){
         RestAssured.baseURI=BaseUrl;
@@ -34,19 +33,9 @@ public class Patterns {
         return response;
     }
 
-    public void VerifyResponseValues(){
+    public List<String> ValuesList(){
         List<String> values = SetRequestHeaders().xmlPath().getList("patterns.pattern.numViews");
-        for (int i=0;i<20;i++){
-            if(Integer.parseInt(values.get(i))>4000){
-                flag = true;
-                System.out.println("NumViews Values of "+" "+i+" "+"Greater Than 4000 are : "+ Integer.parseInt(values.get(i)));
-            }else
-            {
-                flag = false;
-            }
-            softAssert.assertTrue(flag);
-        }
-        softAssert.assertAll();
+        return values;
     }
 
 }
--- src/test/java/StepDefinition/NumViews.java
+++ src/test/java/StepDefinition/NumViews.java
@@ -3,9 +3,11 @@ package StepDefinition;
 import POM.Patterns;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.testng.asserts.SoftAssert;
 
 public class NumViews {
-
+    boolean flag;
+    SoftAssert softAssert = new SoftAssert();
     Patterns patterns = new Patterns();
 
     @Given("User Go To Patterns service api endpoint")
@@ -18,9 +20,26 @@ public class NumViews {
        patterns.SetRequestHeaders();
     }
 
-    @Then("Verify numViews Values Greater Than 4000")
-    public void verifyNumViewsValuesGreaterThan() {
-       patterns.VerifyResponseValues();
+    @Then("Verify numViews Values Greater Than {int}")
+    public void verifyNumViewsValuesGreaterThan(int value) {
+        /*int[] intArray= new int[patterns.ValuesList().size()];
+        int count=0;*/
+        for (int i=0;i<20;i++){
+            if(Integer.parseInt(patterns.ValuesList().get(i))>value){
+              /*  intArray[count]=Integer.parseInt(patterns.ValuesList().get(i));
+                count++;*/
+                flag = true;
+                System.out.println("NumViews Values of "+" "+i+" "+"Greater Than "+value+          " are : "+
+                        Integer.parseInt(patterns.ValuesList().get(i)));
+            }else
+            {
+                flag = false;
+                System.out.println("NumViews Values of "+" "+i+" "+"less than "+ value+" :" +
+                        Integer.parseInt(patterns.ValuesList().get(i)));
+            }
+            softAssert.assertTrue(flag);
+        }
+        softAssert.assertAll();
     }
 
 
